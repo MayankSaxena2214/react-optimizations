026a841ac4b922ab5a1bd7bf9f4974c9c62e23cc
Mayank Saxena <mayanksaxena2214@gmail.com>
2025-08-03 20:35:24 +0530

    usememo done

--- src/components/Parent.jsx
+++ src/components/Parent.jsx
@@ -1,15 +1,27 @@
-import React, { useState } from 'react'
-import Child from './Child';
+import React, { useMemo, useState } from 'react'
+
 
 const Parent = () => {
     const [count,setCount]=useState(0);
-    const [childRender,setChildRender]=useState(true);
+    const [val1,setVal1]=useState(1);
+    const [val2,setVal2]=useState(2);
+    const expensiveCalculation=useMemo(()=>{
+        console.log("Calculating again");
+        return val1+val2;
+    },[val1,val2]);
+
+    const expensiveWithoutMemo=()=>{
+        console.log("INside witnout memo");
+        return val1+val2;
+    };
   return (
     <div>
       <h1>Count is {count}</h1>
         <button onClick={()=>setCount(count+1)}>Increment </button>
-        <button onClick={()=>setChildRender(!childRender)}>Render child</button>
-      <Child childRender={childRender}/>
+        <button onClick={()=>setVal1(val1+1)}>Inc Val1</button>
+        <button onClick={()=>setVal2(val2+1)}>Inc Val2</button>
+        <h1>Expensive result is: {expensiveCalculation}</h1>
+        <h1>Expensive without memo  is: {expensiveWithoutMemo()}</h1>
     </div>
   )
 }
